b0a9deb28167927d6fe0309b9a62d926d68385cd
xichen <xichenliu1994@gmail.com>
2018-07-27 16:37:07 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/xichenliu/myweather/db/Province.java
@@ -0,0 +1,33 @@
+package com.xichenliu.myweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport{
+    private int id;//实体类字段
+    private String provinceName;//省的名字
+    private int provinceCode;//省的代号
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
